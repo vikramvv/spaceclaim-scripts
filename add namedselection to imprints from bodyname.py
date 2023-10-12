ce88424f0235931f9b32866dae8fc6f53c0e2b89
vikramvv <123736880+vikramvv@users.noreply.github.com>
2023-10-12 00:53:12 -0400

    Update add namedselection to imprints from bodyname.py

--- add namedselection to imprints from bodyname.py
+++ add namedselection to imprints from bodyname.py
@@ -1,9 +1,19 @@
-# Python Script, API Version = V23
+"""
+Geometry Simplification and Imprinting Script for Ansys Discovery
+
+This Python script simplifies geometry by fixing extra edges and imprints curves and faces.
+
+Author: vikramv
+License: MIT License (https://opensource.org/licenses/MIT)
+
+API Version: V23
+"""
 
 # Simplify Object
 result = FixExtraEdges.FindAndFix(Info5)
-# EndBlock
+
 secondarySelection = Selection.Empty()
+
 # Imprint Curves
 options = FixImprintOptions()
 options.Tolerance = MM(0.1)
@@ -12,9 +22,13 @@ options.SearchEdges = True
 options.SearchCurves = True
 options.ImprintInstanceType = ImprintInstanceType.IgnoreInstances
 result = FixImprint.FindAndFix(options, Info5)
-# EndBlock
 
-imprintface=FaceSelection.Create(ImprintEdgesResult(success,Info5).CreatedFaces)
+# Create a named selection for the imprinted faces
+imprintface = FaceSelection.Create(ImprintEdgesResult(success, Info5).CreatedFaces)
 result = NamedSelection.Create(imprintface.FilterMinArea(), secondarySelection)
 result = NamedSelection.Rename("Group1", "tt")
-# EndBlock
+
+# Include a description of what the script does in a README file.
+# Specify the license in a LICENSE file (e.g., MIT License).
+
+# End of the script
